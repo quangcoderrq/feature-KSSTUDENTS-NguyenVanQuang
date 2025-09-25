6105019cf8b85bcfaee3e167bd61445aab82c08a
quangcoderrq <nhanthiengg123@gmail.com>
2025-09-25 18:36:12 +0700

    feat: add Fibonacci interface inside module1

--- NguyenVanQuang/module1/Fibonacci.kt
+++ NguyenVanQuang/module1/Fibonacci.kt
@@ -1,1 +1,20 @@
 
+package nguyenvanquang.module1
+
+interface IFibonacci {
+    fun fibonacci(n: Int, flag: Boolean): Long
+}
+
+class FibonacciImpl : IFibonacci {
+    override fun fibonacci(n: Int, flag: Boolean): Long {
+        return if (flag) recursiveNotImplemented(n) else iterativeNotImplemented(n)
+    }
+
+    private fun recursiveNotImplemented(n: Int): Long {
+        throw UnsupportedOperationException("Recursive not implemented yet")
+    }
+
+    private fun iterativeNotImplemented(n: Int): Long {
+        throw UnsupportedOperationException("Iterative not implemented yet")
+    }
+}
